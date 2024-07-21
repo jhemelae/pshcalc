7877c707d2798d255626c6e66bca684f2e756c5b
Jens Hemelaer <hemelaerjens@gmail.com>
2024-07-21 17:25:56 +0200

    with streaming iterator, 12s (?)

--- examples/semigroup.rs
+++ examples/semigroup.rs
@@ -1,17 +1,59 @@
 use std::time::Instant;
 
-#[inline(always)]
-fn next(x: &mut [usize], n: usize) {
-    for i in 0..x.len() {
-        // Unsafe block to avoid bounds checks within the loop
-        unsafe {
-            let elem = x.get_unchecked_mut(i);
-            *elem += 1;
-            if *elem == n {
-                *elem = 0;
-            } else {
-                break;
+use streaming_iterator::StreamingIterator;
+
+enum IteratorState {
+    Start,
+    Running,
+    End,
+}
+
+struct TupleStreamingIterator {
+    state: IteratorState,
+    current: Vec<usize>,
+    sizes: Vec<usize>,
+}
+
+impl TupleStreamingIterator {
+    fn new(sizes: Vec<usize>) -> Self {
+        let current = vec![0; sizes.len()];
+        Self {
+            state: IteratorState::Start,
+            current,
+            sizes,
+        }
+    }
+}
+
+impl StreamingIterator for TupleStreamingIterator {
+    type Item = Vec<usize>;
+
+    fn advance(&mut self) {
+        match self.state {
+            IteratorState::Start => {
+                self.state = IteratorState::Running;
             }
+            IteratorState::Running => {
+                let array = &mut self.current;
+                for i in 0..array.len() {
+                    array[i] += 1;
+                    if array[i] == self.sizes[i] {
+                        array[i] = 0;
+                    } else {
+                        return;
+                    }
+                }
+                self.state = IteratorState::End;
+            }
+            IteratorState::End => {}
+        }
+    }
+
+    fn get(&self) -> Option<&Self::Item> {
+        match self.state {
+            IteratorState::Start => None,
+            IteratorState::Running => Some(&self.current),
+            IteratorState::End => None,
         }
     }
 }
@@ -41,15 +83,16 @@ fn main() {
     let start = Instant::now();
     let n: usize = 4;
     let size = n * n;
-    
-    let mut array = vec![0; size];
+
+    let mut iterator = TupleStreamingIterator::new(
+        vec![n;size]
+    );
 
     let mut count = 0;
-    for _i in 0..n.pow(size as u32) {
+    while let Some(array) = iterator.next() {
         if is_associative(&array, n) {
             count += 1;
         }
-        next(&mut array, n);
     }
     println!("Count = {:?}", count);
 
